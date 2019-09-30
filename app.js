0c9309f00d1cc37838fcbf825b1ab194b5fb86ee
jokodoepat <jkuncoro28@gmail.com>
2019-09-30 10:50:09 +0700

    update

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    var x, y, z
+    x = 5
+    y = 3
+    z = x + y
+
+    return res.send(`The value of z is ${z}`)
+})
+
+app.post('/', (req, res) => {
+    let email = req.body.email
+    
+    return res.send(`The email value is ${email}`)
+})
+
+app.listen(3300, () => {
+    console.log(`Example app listening on port 3300`)
+})
